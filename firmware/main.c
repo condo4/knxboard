9a944e32f93e544017a1c00fda06c302253b8e4b
Fabien Proriol <fabien.proriol@kazoe.org>
2022-12-07 15:06:21 +0100

    State: Led Blink on both Nucleo and KnxBoardV3

--- firmware/main.c
+++ firmware/main.c
@@ -1,15 +1,34 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
 
-int main(void) {
-	rcc_periph_clock_enable(RCC_GPIOB);
+
+
+static void setup_knx_led(void)
+{
+	rcc_periph_clock_enable(KNXGPIOLED_RCC);
 	gpio_mode_setup(KNXGPIOLED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, KNXGPIOLED_PIN);
-	gpio_set(KNXGPIOLED_PORT, KNXGPIOLED_PIN);
+}
+
+static void setup_debug_uart(void)
+{
+	rcc_periph_clock_enable(KNXGPIOLED_RCC);
+	gpio_mode_setup(KNXGPIOLED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, KNXGPIOLED_PIN);
+}
+
+
+
+int main(void) {
+	setup_knx_led();
+
 	while(1) {
 		/* wait a little bit */
-		for (int i = 0; i < LITTLE_BIT; i++) {
+		for (int i = 0; i < LITTLE_BIT * 10; i++) {
 			__asm__("nop");
 		}
-		gpio_toggle(KNXGPIOLED_PORT, KNXGPIOLED_PIN);
+		gpio_set(KNXGPIOLED_PORT, KNXGPIOLED_PIN);
+		for (int i = 0; i < LITTLE_BIT / 2; i++) {
+			__asm__("nop");
+		}
+		gpio_clear(KNXGPIOLED_PORT, KNXGPIOLED_PIN);
 	}
 }
