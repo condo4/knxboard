4c948c20268ebbd34539a5da4266d99960de8ff5
Fabien Proriol <fabien.proriol@kazoe.org>
2023-01-01 13:34:06 +0100

    Blink also on STM32G4

--- firmware/main.c
+++ firmware/main.c
@@ -18,19 +18,20 @@ void _write(void)
 
 int main(void){
     static uint32_t last_tick = 0;
+    static uint32_t wait = 50;
     HAL_Init();
-    MX_DMA_Init();
+    //MX_DMA_Init();
     MX_USART1_UART_Init();
     MX_USART2_UART_Init();
     SystemClock_Config();
     MX_GPIO_Init();
-    MX_ADC1_Init();
+    //MX_ADC1_Init();
     
     HAL_GPIO_WritePin(KNX_PROG_LED_GPIO_Port, KNX_PROG_LED_Pin, GPIO_PIN_SET);
     
     while (1){
         
-        if(HAL_GetTick() > (last_tick + 500) || last_tick >  HAL_GetTick())/* Meas each 5s */
+        if(HAL_GetTick() > (last_tick + wait) || last_tick >  HAL_GetTick())/* Meas each 5s */
         {
             last_tick = HAL_GetTick();
             HAL_GPIO_TogglePin(KNX_PROG_LED_GPIO_Port, KNX_PROG_LED_Pin);
